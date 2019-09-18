3144a7d3f45dd747f1e22f9ddf4cea0a229af226
yvolorenzo2019 <yvolorezo@gmail.com>
2019-09-19 04:53:10 +0900

    Project's Initial Commit

---
+++ www/js/cadastrar.js
@@ -0,0 +1,16 @@
+$(function(){
+  $('#entrar').click(function(){
+     $(location).attr('href', 'eventos.html');
+  });
+  
+});
+
+function abrirmenu() {
+  document.getElementById("mySidebar").style.display = "block";
+}
+function fechar() {
+  document.getElementById("mySidebar").style.display = "none";
+}
+ 
+
+
