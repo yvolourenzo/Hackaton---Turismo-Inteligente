6f7ed562185bd8348cd3bf565e170d2304aa4280
PedroH123 <pedro_3ano@hotmail.com>
2019-09-20 04:27:56 +0900

    Criação do Login e Senha \(Login: adm\) \(Senha: 1234\) + Criação da Tela de adicionar Evento

--- www/js/cadastrar.js
+++ www/js/cadastrar.js
@@ -1,6 +1,26 @@
 $(function(){
   $('#entrar').click(function(){
+
+    if($('#login').val() == 'adm' && $('#senha').val() == '1234'){
+
      $(location).attr('href', 'eventos.html');
+
+  }
+
+    if($('#login').val() != 'adm' && $('#senha').val() == '1234'){
+
+   alert('Login Incorreto');
+
+  }
+  if($('#login').val() == 'adm' && $('#senha').val() != '1234'){
+
+   alert('Senha Incorreta');
+  }
+  if($('#login').val() != 'adm' && $('#senha').val() != '1234'){
+
+alert('Login e Senha Incorretos');
+
+  }
   });
 
 });
